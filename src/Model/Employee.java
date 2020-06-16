f3f240159df1bebab3d335baf34956faf1afe02e
Mohammad YousefiPour <moyousefipour79@gmail.com>
2020-06-16 17:45:29 +0430

    Make Employee, Manager abd Passenger classes

---
+++ src/Model/Employee.java
@@ -0,0 +1,38 @@
+package Model;
+
+public class Employee extends Person implements Showable {
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getLastname() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getEmail() {
+        return null;
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
