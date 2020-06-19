a853562ebe7852f4a7b90c1cbab6b282ff6e0783
myp79 <moyousefipour79@gmail.com>
2020-06-20 03:24:09 +0430

    Edit method of Employee, Manager and Passenger classes that reference to Person class

--- src/Model/Manager.java
+++ src/Model/Manager.java
@@ -2,36 +2,6 @@ package Model;
 
 public class Manager extends Person implements Showable{
     @Override
-    public String getId() {
-        return null;
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public String getLastname() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getEmail() {
-        return null;
-    }
-
-    @Override
     public void show() {
 
     }
--- src/Model/Passenger.java
+++ src/Model/Passenger.java
@@ -1,35 +1,6 @@
 package Model;
 
 public class Passenger extends Person implements Showable{
-    @Override
-    public String getId() {
-        return null;
-    }
-
-    @Override
-    public String getName() {
-        return null;
-    }
-
-    @Override
-    public String getLastname() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getEmail() {
-        return null;
-    }
 
     @Override
     public void show() {
