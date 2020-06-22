f2b410075d0363424e3b41843da85e30e3170fe3
myp79 <moyousefipour79@gmail.com>
2020-06-22 07:04:45 +0430

    check Email and set alert for it

--- src/Controller/SignUpController.java
+++ src/Controller/SignUpController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Model.Database;
 import View.SignUpView;
+import javafx.scene.control.Alert;
 
 public class SignUpController {
     SignUpView signUpView;
@@ -19,7 +20,15 @@ public class SignUpController {
             String idcard = signUpView.getIdCard().getText();
             String username = signUpView.getUsername().getText();
             String password = signUpView.getPassword().getText();
-            Database.add(name, lastname, email, idcard, username, password, "passenger");
+            if (email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])")){
+                Database.add(name, lastname, email, idcard, username, password, "passenger");
+            }
+            else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Email Error");
+                alert.setContentText("Email has wrong pattern. Check it.");
+                alert.show();
+            }
         });
     }
 
