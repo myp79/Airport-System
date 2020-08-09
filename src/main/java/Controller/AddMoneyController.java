d34e28ec190d15c677a6ebf0a90fadffdb84b714
myp79 <moyousefipour79@gmail.com>
2020-08-09 21:01:29 +0430

    Alert some error in AddMoneyController

--- src/main/java/Controller/AddMoneyController.java
+++ src/main/java/Controller/AddMoneyController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Model.Database;
 import View.AddMoney;
+import javafx.scene.control.Alert;
 
 public class AddMoneyController {
     private AddMoney addMoney;
@@ -18,13 +19,20 @@ public class AddMoneyController {
 
     public void submitBtn() {
         addMoney.getSubmit().setOnAction(actionEvent -> {
-            int money = Integer.parseInt(addMoney.getMoney().getText());
-            Database.update(username,money);
+            String money = addMoney.getMoney().getText();
+            if (money.matches("\\d+\\D+")) {
+                Database.update(username, Integer.parseInt(money));
+            } else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Money Error");
+                alert.setContentText("Money have word and number. Check it.");
+                alert.show();
+            }
         });
     }
 
     public void setUsername(String username) {
         this.username = username;
     }
-    
+
 }
