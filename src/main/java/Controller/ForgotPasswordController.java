604cf9d06159eb981410b353e8bf35bac59ed121
myp79 <moyousefipour79@gmail.com>
2020-08-09 19:17:43 +0430

    ForgotPasswordController authentication check

--- src/main/java/Controller/ForgotPasswordController.java
+++ src/main/java/Controller/ForgotPasswordController.java
@@ -17,15 +17,22 @@ public class ForgotPasswordController {
         forgotPassword.getSubmit().setOnAction(actionEvent -> {
             String username = forgotPassword.getUsername().getText();
             String email = forgotPassword.getEmail().getText();
-            String password = Database.forgotPassword(username, email);
-            if (password != null) {
-                LoginController loginController = new LoginController();
-                loginController.getLoginView().getPassword().setText(password);
-                forgotPassword.getScene().setRoot(loginController.getLoginView());
+            if (email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])")) {
+                String password = Database.forgotPassword(username, email);
+                if (password != null) {
+                    LoginController loginController = new LoginController();
+                    loginController.getLoginView().getPassword().setText(password);
+                    forgotPassword.getScene().setRoot(loginController.getLoginView());
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Database Error");
+                    alert.setContentText("User doesn't exist. Check username and email.");
+                    alert.show();
+                }
             } else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Database Error");
-                alert.setContentText("User doesn't exist. Check username and email.");
+                alert.setTitle("Email Error");
+                alert.setContentText("Email has wrong pattern. Check it.");
                 alert.show();
             }
         });
