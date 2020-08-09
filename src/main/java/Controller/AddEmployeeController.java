264fa1e978be8b08cb3ed6870ce395277bd9bd1d
myp79 <moyousefipour79@gmail.com>
2020-08-09 19:41:35 +0430

    Money authentication

--- src/main/java/Controller/AddEmployeeController.java
+++ src/main/java/Controller/AddEmployeeController.java
@@ -28,9 +28,16 @@ public class AddEmployeeController {
                     if (lastname.matches("\\D+")) {
                         if (email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])")) {
                             if (!idcard.matches("\\d+\\D+")) {
-                                Database.add(name, lastname, email, address, idcard, Integer.parseInt(money), username, password, "employee");
-                                EmployeeManageController employeeManageController = new EmployeeManageController();
-                                addEmployee.getScene().setRoot(employeeManageController.getEmployeeManage());
+                                if (!money.matches("\\d+\\D+")) {
+                                    Database.add(name, lastname, email, address, idcard, Integer.parseInt(money), username, password, "employee");
+                                    EmployeeManageController employeeManageController = new EmployeeManageController();
+                                    addEmployee.getScene().setRoot(employeeManageController.getEmployeeManage());
+                                } else {
+                                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                                    alert.setTitle("Money Error");
+                                    alert.setContentText("ID have number and word. Check it.");
+                                    alert.show();
+                                }
                             } else {
                                 Alert alert = new Alert(Alert.AlertType.ERROR);
                                 alert.setTitle("ID Error");
