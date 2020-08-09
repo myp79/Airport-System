c375ec7d22ca59dac6a993d46b1560a165b88824
myp79 <moyousefipour79@gmail.com>
2020-08-09 19:32:30 +0430

    AddEmployeeController authentication check

--- src/main/java/Controller/AddEmployeeController.java
+++ src/main/java/Controller/AddEmployeeController.java
@@ -22,14 +22,42 @@ public class AddEmployeeController {
             String username = addEmployee.getUsername().getText();
             String password = addEmployee.getPassword().getText();
             String address = addEmployee.getAddress().getText();
-            if (email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])")) {
-                Database.add(name, lastname, email, address, idcard, 0, username, password, "employee");
-                EmployeeManageController employeeManageController = new EmployeeManageController();
-                addEmployee.getScene().setRoot(employeeManageController.getEmployeeManage());
+            if (!name.equals("") && !lastname.equals("") && !email.equals("") && !idcard.equals("") && !username.equals("") && !password.equals("") && !address.equals("")) {
+                if (name.matches("\\D+")) {
+                    if (lastname.matches("\\D+")) {
+                        if (email.matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)])")) {
+                            if (!idcard.matches("\\d+\\D+")) {
+                                Database.add(name, lastname, email, address, idcard, 0, username, password, "employee");
+                                EmployeeManageController employeeManageController = new EmployeeManageController();
+                                addEmployee.getScene().setRoot(employeeManageController.getEmployeeManage());
+                            } else {
+                                Alert alert = new Alert(Alert.AlertType.ERROR);
+                                alert.setTitle("ID Error");
+                                alert.setContentText("ID have number and word. Check it.");
+                                alert.show();
+                            }
+                        } else {
+                            Alert alert = new Alert(Alert.AlertType.ERROR);
+                            alert.setTitle("Email Error");
+                            alert.setContentText("Email has wrong pattern. Check it.");
+                            alert.show();
+                        }
+                    } else {
+                        Alert alert = new Alert(Alert.AlertType.ERROR);
+                        alert.setTitle("LastName Error");
+                        alert.setContentText("LastName has number . Check it.");
+                        alert.show();
+                    }
+                } else {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Name Error");
+                    alert.setContentText("Name has number . Check it.");
+                    alert.show();
+                }
             } else {
                 Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle("Email Error");
-                alert.setContentText("Email has wrong pattern. Check it.");
+                alert.setTitle("Null Error");
+                alert.setContentText("All field required . Check it.");
                 alert.show();
             }
         });
